149d7f93b782645c753b3f36d0b47a270e0762ee
Tim Sayler <tsayler@gmail.com>
2016-01-15 12:56:49 -0800

    Added optional getUserInfo2() function.

--- main.c
+++ main.c
@@ -15,10 +15,35 @@ int getUserInfo(char name[]) {
     return luck;
 }
 
+int getUserInfo2(char name[]) {
+    char fname[48];
+    char lname[48];
+    char msg[100];
+    char fullName[96];
+    size_t length;
+    int luck;
+    sprintf(msg, "Enter your last name followed by your first name: ");
+    puts(msg);
+    scanf("%s %s", lname,fname);
+    sprintf(fullName, "%s %s", fname, lname);
+    length = ((sizeof(char) *strlen(fullName))+1);
+    memcpy(name,fullName, length);
+    sprintf(msg, "What's your lucky number?");
+    puts(msg);
+    scanf("%d", &luck);
+    return luck;
+}
+
 int main() {
     char fullname[100];
+    char msg[99];
+    //version 1, using strcpy and strcat
     int lucky = getUserInfo(fullname);
     printf("%s's lucky number is %d.\n", fullname, lucky);
+    //version 2, using sprintf, strlen and memcpy
+    lucky = getUserInfo2(fullname);
+    sprintf(msg, "%s's lucky number is %d.\n", fullname, lucky);
+    puts(msg);
 
     return 0;
 }
